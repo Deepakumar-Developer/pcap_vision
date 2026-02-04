5dff65a2b71f16501269dc541f2bc6caf8a4d98c
Deepakumar-Developer <deepakumardeveloper@gmail.com>
2026-02-04 21:35:49 +0530

    v1.0.1

--- pcap_api/app.py
+++ pcap_api/app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_cors import CORS
 
 app = Flask(__name__)
-(CORS(app)
 
- @app.route('/home'))
-def home():
-    return "This is the home page"
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
